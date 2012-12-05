59d5041b5d0fb052c9629f3c36be853d4703f58f
Matthew A. Miller <linuxwolf@outer-planes.net>
2012-12-05 13:31:59 -0700

    more testing of session

--- test/test-session.js
+++ test/test-session.js
@@ -5,44 +5,29 @@
 var promised_io = require("promised-io"),
     SASLSession = require("../lib/session.js").SASLSession;
 
-var mockClientMech = {
-    "name": "MOCK",
-    "stepStart" : function(config) {
-        var deferred = new promised_io.Deferred();
-        deferred.resolve(new Buffer("client initial"));
-        return deferred.promise;
-    }
-};
-var mockServerMech = {
-    "name":"MOCK",
-    "stepStart": function(config, input) {
-        var deferred = new promised_io.Deferred();
-        deferred.resolve(new Buffer("server initial"));
-        return deferred.promise;
-    }
-}
-
 module.exports = {
-    setUp: function(cb) {
-        cb();
-    },
-    tearDown: function(cb) {
-        cb();
-    },
     "test create" : function(test) {
+        var mech = {
+            "name": "mech",
+            "stepStart" : function(config) {
+                var deferred = new promised_io.Deferred();
+                deferred.resolve(new Buffer("client initial"));
+                return deferred.promise;
+            }
+        };
         var config,
             session;
 
         config = {};
-        session = new SASLSession(mockClientMech, config);
-        test.equal(session.mechanism, mockClientMech.name);
+        session = new SASLSession(mech, config);
+        test.equal(session.mechanism, mech.name);
         test.notStrictEqual(session.config, config);
         test.ok(!session.completed);
         test.equal(session.config.state, "start");
 
         config = { state:"complete" };
-        session = new SASLSession(mockClientMech, config);
-        test.equal(session.mechanism, mockClientMech.name);
+        session = new SASLSession(mech, config);
+        test.equal(session.mechanism, mech.name);
         test.notStrictEqual(session.config, config);
         test.ok(!session.completed);
         test.equal(session.config.state, "start");
@@ -51,15 +36,15 @@ module.exports = {
             username:"bilbo.baggins",
             password:"! 84G3nd"
         };
-        session = new SASLSession(mockClientMech, config);
+        session = new SASLSession(mech, config);
         config.state = "start";
-        test.equal(session.mechanism, mockClientMech.name);
+        test.equal(session.mechanism, mech.name);
         test.notStrictEqual(session.config, config);
         test.ok(!session.completed);
         test.deepEqual(session.config, config);
 
-        session = new SASLSession(mockClientMech);
-        test.equal(session.mechanism, mockClientMech.name);
+        session = new SASLSession(mech);
+        test.equal(session.mechanism, mech.name);
         test.ok(session.config);
         test.ok(!session.completed);
         test.equal(session.config.state, "start");
@@ -127,6 +112,79 @@ module.exports = {
             test.done();
         }, function(err) {
             test.fail(err && err.message);
+            test.done();
+        });
+    },
+    "test step - client single-stage failure (from mech)": function(test) {
+        var config,
+            session;
+        var mech = {
+            name : "MOCK-MECH",
+            stepStart: function(config, input) {
+                test.strictEqual(config, session.config);
+                test.ok(!input);
+                test.equal(config.state, "start");
+
+                var deferred = new promised_io.Deferred();
+                deferred.reject(new Error("mechanism failure"));
+                return deferred.promise;
+            }
+        };
+        config = {};
+        session = new SASLSession(mech, config);
+        var promise = session.step();
+        test.ok(promise);
+        test.equal(typeof(promise.then), "function");
+        promise.then(function(output) {
+            test.fail();
+            test.done();
+        }, function(err) {
+            test.ok(err instanceof Error);
+            test.equal(err.message, "mechanism failure");
+            test.ok(session.completed);
+            test.done();
+        });
+    },
+    "test step - client single-stage failure (invalid state)": function(test) {
+        var config,
+            session;
+        var mech = {
+            name : "MOCK-MECH",
+            stepStart: function(config, input) {
+                test.strictEqual(config, session.config);
+                test.ok(!input);
+                test.equal(config.state, "start");
+
+                config.state = "complete";
+                var deferred = new promised_io.Deferred();
+                deferred.resolve(new Buffer("client initial"));
+                return deferred.promise;
+            }
+        };
+        config = {};
+        session = new SASLSession(mech, config);
+        var promise = session.step();
+        test.ok(promise);
+        test.equal(typeof(promise.then), "function");
+        promise.then(function(output) {
+            test.ok(output instanceof Buffer);
+            test.equal(output.toString(), "client initial");
+            test.ok(session.completed);
+        }, function(err) {
+            test.fail(err && err.message);
+            test.done();
+        });
+
+        promise = session.step();
+        test.ok(promise);
+        test.equal(typeof(promise.then), "function");
+        promise.then(function(output) {
+            test.fail();
+            test.done();
+        }, function(err) {
+            test.ok(err instanceof Error);
+            test.equal(err.message, "invalid state");
+            test.ok(session.completed);
             test.done();
         });
     },
