5f6323d07df01a1865a8ae0d95b82ebd482fffca
Matthew A. Miller <linuxwolf@outer-planes.net>
2012-12-11 17:58:00 -0700

    initial SCRAM-SHA1 server mech (NO TESTING)

--- scram-sha1.js
+++ scram-sha1.js
@@ -15,7 +15,6 @@ var _XOR = function(s1, s2, encoding) {
 
     return out.toString(encoding || "binary");
 }
-exports.XOR = _XOR;
 
 var calcSaltedPwd = function(pwd, salt, itrs) {
     var U;
@@ -34,7 +33,6 @@ var calcSaltedPwd = function(pwd, salt, itrs) {
 
     return U;
 }
-exports.pbkdf2 = calcSaltedPwd;
 
 var calcClientKey = function(spwd) {
     return crypto.createHmac("sha1", spwd).
@@ -65,6 +63,8 @@ var calcServerSig = function(key, msg) {
                   update(msg).
                   digest("binary");
 }
+
+var PTN_FIELD = /([^\=]+)\=(.+)/g;
 
 exports.client = {
     name: "SCRAM-SHA1",
@@ -137,7 +137,7 @@ exports.client = {
                 extra = [];
             input = input.split(",");
             input.forEach(function(f) {
-                f = /([^\=]+)\=(.+)/g.exec(f);
+                f = PTN_FIELD.exec(f);
 
                 var fIdx = fields.indexOf(f[1]);
                 if (fIdx !== -1) {
@@ -226,7 +226,7 @@ exports.client = {
             var verification;
             input = input.split(",");
             input.forEach(function(f) {
-                f = /([^\=]+)\=(.+)/g.exec(f);
+                f = PTN_FIELD.exec(f);
 
                 switch(f[1]) {
                     case "v":
@@ -262,4 +262,208 @@ exports.client = {
 
         return deferred.promise;
     }
-}
+};
+exports.server = {
+    name:"SCRAM-SHA1",
+    stepStart: function(config, input) {
+        input = input.toString("binary");
+
+        var deferred = new promised_io.Deferred();
+        promised_io.seq([
+            function() { return helpers.promisedValue(config, "username"); },
+            function(usr) {
+                return promised_io.all(
+                    promised_io.whenPromise(usr),
+                    helpers.promisedValue(config, "nonce"),
+                    helpers.promisedValue(config, "iterations", usr),
+                    helpers.promisedValue(config, "salt", usr)
+                );
+            }
+        ]).then(function(factors) {
+            var info = config.authScram = {};
+            var cfgUsr = factors[0],
+                cfgNonce = factors[1],
+                cfgItrs = factors[2],
+                cfgSalt = factors[3];
+
+            // deconstruct input
+            input = input.split(",");
+
+            // input[0] == binding request
+            if (input[0] !== "n") {
+                deferred.reject(new Error("channel binding not supported"));
+                return;
+            }
+
+            input = input.slice(1);
+            input.forEach(function(f, i) {
+                f = PTN_FIELD.exec(f);
+                if (!f) {
+                    return;
+                }
+
+                switch(f[1]) {
+                    case "n":
+                        info.username = f[2];
+                        break;
+                    case "r":
+                        info.nonce = f[2];
+                        break;
+                    case "a":
+                        info.authzid = f[2];
+                        break;
+                }
+            });
+
+            // validate username
+            if (cfgUsr && cfgUsr !== info.username) {
+                deferred.reject(new Error("invalid username"));
+                return;
+            }
+
+            // remember messages
+            info.messages = [];
+            // recalculate client-first-message-bare
+            info.messages.push([
+                "n=" + info.username,
+                "r=" + info.nonce
+            ].join(","));
+
+            // calculate nonce/salt/iterations
+            if (!cfgNonce) {
+                cfgNonce = crypto.randomBytes(24).
+                                  toString("base64");
+            }
+            info.nonce = info.nonce + cfgNonce;
+
+            if (!cfgSalt) {
+                cfgSalt = crypto.randomBytes(16).
+                                 toString("binary");
+            }
+            info.salt = cfgSalt;
+
+            if (!cfgItrs) {
+                cfgItrs = 4096;
+            }
+            info.iterations = cfgItrs;
+
+            // pre-calculate binding
+            info.binding = [
+                "n",
+                info.authzid ? "a=" + info.authzid : "",
+                ""
+            ].join(",");
+            info.binding = new Buffer(info.binding, "binary").
+                           toString("base64");
+
+            var data = [
+                "r=" + info.nonce,
+                "s=" + new Buffer(info.salt, "binary").toString("base64"),
+                "i=" + info.iterations
+            ].join(",");
+
+            // remember server-first-message
+            info.messages.push(data);
+
+            deferred.resolve({
+                state:"auth",
+                data:data
+            });
+        }, function(err) {
+            deferred.reject(err);
+        });
+
+        return deferred.promise;
+    },
+    stepAuth: function(config, input) {
+        var info = config.authScram;
+        input = input.toString("binary");
+
+        promised_io.all(
+            helpers.promisedValue(config, "password", info.username),
+            helpers.promisedValue(config, "authzid", info.username)
+        ).then(function(factors) {
+            var cfgPwd = factors[0],
+                cfgAuthzid = factors[1],
+                cfgBinding = info.binding,
+                cfgNonce = info.nonce;
+            var usrProof;
+
+            // remove pre-conceived notions
+            delete info.binding;
+            delete info.nonce;
+
+            // deconstruct input
+            input = input.toString("binary").
+                          split(",");
+            input.forEach(function(f) {
+                f = PTN_FIELD.exec(f);
+
+                switch(f[1]) {
+                    case "c":
+                        info.binding = f[2];
+                        break;
+                    case "r":
+                        info.nonce = f[2];
+                        break;
+                    case "p":
+                        usrProof = f[2];
+                        break;
+                }
+            });
+
+            // validate binding/nonce
+            if (info.binding !== cfgBinding) {
+                deferred.reject(new Error("invalid binding"));
+                return;
+            }
+            if (info.nonce !== cfgNonce) {
+                deferred.reject(new Error("invalid nonce"));
+                return;
+            }
+
+            // recalculate client-final-message-without-proof
+            info.messages.push([
+                "c=" + info.binding,
+                "r=" + info.nonce
+            ].join(","));
+
+            // calculate proof
+            var pwd, key, sig, proof;
+            pwd = calcSaltedPwd(cfgPwd,
+                                info.salt,
+                                info.iterations);
+            key = calcClientKey(pwd);
+            sig = calcClientSig(key, info.messages.join(","));
+            proof = calcClientProof(key, sig);
+            if (proof !== usrProof) {
+                deferred.reject(new Error("not authorized"));
+                return;
+            }
+
+            // validate authzid
+            if (    cfgAuthzid &&
+                    info.authzid &&
+                    cfgAuthzid !== info.authzid) {
+                deferred.reject(new Error("not authorized"));
+                return;
+            }
+
+            // calculate ServerSignature
+            key = calcServerKey(pwd);
+            sig = calcServerSig(key, info.messages.join(","));
+
+            var data = "v=" + new Buffer(sig, "binary").toString("base64");
+            deferred.resolve({
+                state:"complete",
+                data:data,
+                username:info.username,
+                authzid:cfgAuthzid || info.authzid || info.username
+            });
+        }, function(err) {
+            deferred.reject(err);
+        });
+
+        return deferred.promise;
+    }
+};
