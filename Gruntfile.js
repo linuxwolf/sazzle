7919c69553293d8c773fdd68b024656f8429a483
Matthew A. Miller <linuxwolf@outer-planes.net>
2014-05-17 08:19:27 -0600

    fix errors in build setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,6 +25,7 @@
 
 module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-contrib-nodeunit");
     grunt.loadNpmTasks("grunt-github-pages");
     grunt.loadNpmTasks("grunt-jsdoc");
